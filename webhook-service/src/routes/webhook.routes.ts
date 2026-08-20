c1735a54269ac8c8fd3632cfe3c8af67c733fdfd
Abhinay Jangde <abhinayjangde@gmail.com>
2026-08-20 08:41:29 +0530

    get single webhook with id

--- webhook-service/src/routes/webhook.routes.ts
+++ webhook-service/src/routes/webhook.routes.ts
@@ -4,30 +4,80 @@ import { prisma } from "../lib/prisma.js";
 const router = Router();
 
 router.post("/", async (req, res) => {
-  try {
-    const { url, secret } = req.body;
+    try {
+        const { url, secret } = req.body;
 
-    if (!url || !secret) {
-      return res.status(400).json({
-        message: "url and secret are required",
-      });
+        if (!url || !secret) {
+            return res.status(400).json({
+                message: "url and secret are required",
+            });
+        }
+
+        const webhook = await prisma.webhookEndpoint.create({
+            data: {
+                url,
+                secret,
+            },
+        });
+
+        return res.status(201).json(webhook);
+    } catch (error) {
+        console.error(error);
+
+        return res.status(500).json({
+            message: "Internal server error",
+        });
+    }
+});
+
+router.get("/", async (req, res) => {
+    try {
+        const webhooks = await prisma.webhookEndpoint.findMany({
+            select: {
+                id: true,
+                url: true,
+                isActive: true,
+                createdAt: true
+            }
+        });
+        return res.status(200).json(webhooks)
+    } catch (error) {
+        console.error(error);
+
+        return res.status(500).json({
+            message: "Internal server error",
+        });
     }
 
-    const webhook = await prisma.webhookEndpoint.create({
-      data: {
-        url,
-        secret,
-      },
-    });
+})
+router.get("/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
 
-    return res.status(201).json(webhook);
-  } catch (error) {
-    console.error(error);
+        const webhooks = await prisma.webhookEndpoint.findMany({
+            where: {
+                id
+            },
+            select: {
+                id: true,
+                url: true,
+                isActive: true,
+                createdAt: true
+            }
+        });
 
-    return res.status(500).json({
-      message: "Internal server error",
-    });
-  }
-});
+        if(webhooks.length < 1){
+            return res.status(404).json({message:"webhook doesn't exists"})
+        }
+        return res.status(200).json(webhooks)
+    } catch (error) {
+        console.error(error);
+
+        return res.status(500).json({
+            message: "Internal server error",
+        });
+    }
+
+})
 
 export default router;
