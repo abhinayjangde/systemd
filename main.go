c68c461181c1d47d967937895da05b1fa82ca02e
Abhinay Jangde <abhinayjangde@gmail.com>
2026-09-11 12:33:12 +0530

    remove singleton implementation and add hello world example

--- main.go
+++ main.go
@@ -1,28 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
-
-type Singleton struct{}
-
-var (
-	instance *Singleton
-	once     sync.Once
-)
-
-func GetSingleton() *Singleton {
-	once.Do(func() {
-		instance = &Singleton{}
-	})
-
-	return instance
-}
-
 func main() {
-	s1 := GetSingleton()
-	s2 := GetSingleton()
 
-	fmt.Println(s1 == s2)
 }
