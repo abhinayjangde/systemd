1244786a6f0fccdffb5ce398bf775d500f68b790
Abhinay Jangde <abhinayjangde@gmail.com>
2026-09-12 13:52:27 +0530

    config is update

--- notification-system/go.mod
+++ notification-system/go.mod
@@ -3,6 +3,7 @@ module github.com/abhinayjangde/notification-system
 go 1.27.0
 
 require (
+	github.com/caarlos0/env/v11 v11.4.1
 	github.com/golang-migrate/migrate/v4 v4.20.1
 	github.com/joho/godotenv v1.5.1
 )
--- notification-system/internal/config/config.go
+++ notification-system/internal/config/config.go
@@ -3,40 +3,27 @@ package config
 
 import (
 	"log"
-	"os"
 
+	"github.com/caarlos0/env/v11"
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
 	Port        string `env:"PORT" envDefault:"8000"`
 	Env         string `env:"ENV" envDefault:"local"`
-	DatabaseUrl string `env:"DATABASE_URL"`
+	DatabaseUrl string `env:"DATABASE_URL,required"`
 }
 
 func MustLoad() *Config {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Warning: .env file not found")
 	}
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		panic("PORT environment variable is not set")
-	}
-	env := os.Getenv("ENV")
-	if env == "" {
-		panic("ENV environment variable is not set")
-	}
+	var cfg Config
 
-	databaseUrl := os.Getenv("DATABASE_URL")
-	if databaseUrl == "" {
-		panic("DATABASE_URL environment variable is not set")
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatal(err)
 	}
-
-	return &Config{
-		Port:        port,
-		Env:         env,
-		DatabaseUrl: databaseUrl,
-	}
+	return &cfg
 }
