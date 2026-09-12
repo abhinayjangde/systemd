55de88b75333b2d680b5da03cba2db00e8478f4b
Abhinay Jangde <abhinayjangde@gmail.com>
2026-09-12 13:41:28 +0530

    setup api and migrate

--- notification-system/cmd/api/main.go
+++ notification-system/cmd/api/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"net/http"
+	"os"
+	"time"
 
+	"log"
+
+	"github.com/abhinayjangde/notification-system/internal/config"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("POST /notifications", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`notification created`))
+	})
+
+	log.Println("starting server", "port", cfg.Port)
+
+	srv := http.Server{
+		Addr:         ":" + cfg.Port,
+		Handler:      mux,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 30,
+		IdleTimeout:  time.Second * 60,
+	}
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatal("server stopped", "err", err)
+		os.Exit(1)
+	}
 }
